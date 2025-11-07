b263eb5fe4db45b96ef395bf8c82972832bcd876
Ari <aristidisk@protonmail.com>
2025-11-07 19:28:33 +0100

    split architecture diagram into dev workflow and cloud architecture

--- assets/cloud-architecture.py
+++ assets/cloud-architecture.py
@@ -10,7 +10,6 @@ from diagrams.aws.storage import S3
 from diagrams.custom import Custom
 from diagrams.oci.devops import APIService
 from diagrams.onprem.iac import Terraform
-from diagrams.onprem.vcs import Git, Github
 
 # Project directories
 ROOT_DIR = Path(__file__).resolve().parent.parent
@@ -19,37 +18,24 @@ DIAGRAMS_DIR = ROOT_DIR / "assets/diagrams"
 
 with Diagram(
     "FXLake: Serverless ETL on AWS\n(pipeline architecture)",
-    filename=str(DIAGRAMS_DIR / "architecture"),
+    filename=str(DIAGRAMS_DIR / "cloud-architecture"),
     show=False,
     direction="LR",
 ):
-    # Developer environment
+    # External factors
     dev = Custom("Developer", str(ICONS_DIR / "dev.jpg"))
-    git = Git("Git\n(Version Control)")
-    github = Github("GitHub\n(Repository Hosting)")
-    terraform = Terraform("(Infrastructure as Code)")
     api_source = APIService("Frankfurter API")
-
-    dev >> Edge(label="Develop") >> git  # Developer pushes to version control
-
-    (
-        git >> Edge(label="Pull") >> github >> Edge(label="Push") >> git
-    )  # Version control interaction with GitHub
-
-    git >> Edge(label="Trigger") >> terraform  # Trigger IaC on code push
+    terraform = Terraform("(Infrastructure as Code)")
 
     with Cluster("AWS Cloud"):
         aws_cloud = Custom(
             "", str(ICONS_DIR / "aws.png")
-        )  # Intentionally left blank label and icon for AWS Cloud
+        )  # Intentionally left blank label and missing icon for AWS cluster representation
+        terraform >> Edge(label="provision") >> aws_cloud  # Provision AWS resources
 
         with Cluster("Orchestration"):
-            step_function = StepFunctions(
-                "Step Functions\n(Workflow Orchestration)"
-            )  # Pipeline orchestration
-            eventbridge = Eventbridge(
-                "EventBridge\n(Event Routing)"
-            )  # Event-driven triggers
+            step_function = StepFunctions("Step Functions\n(Workflow Orchestration)")
+            eventbridge = Eventbridge("EventBridge\n(Event Routing)")
 
         with Cluster("ETL Pipeline"):
             lambda_function = Lambda("Lambda\n(Serverless Compute)")
@@ -61,6 +47,12 @@ with Diagram(
             s3_processed = S3("S3 Processed Bucket\n(Processed Data)")
             s3_athena_results = S3("S3 Athena Results\n(Sample Queries)")
 
+        with Cluster("Monitoring & Security"):
+            cloudwatch = Cloudwatch("CloudWatch\n(Monitoring)")
+            cloudtrail = Cloudtrail("CloudTrail\n(Audit Logs)")
+            iam = IAM("IAM\n(Access Management)")
+            sns = SNS("SNS\n(Notification Service)")
+
         # Orchestration
         (
             eventbridge
@@ -68,30 +60,32 @@ with Diagram(
             >> step_function
             >> [lambda_function, glue, athena]
         )
+
         # Data flow
         (
             api_source >> lambda_function >> Edge(label="extract") >> s3_raw
         )  # Extract data via Lambda and Frankfurter API
         (
             s3_raw
-            >> Edge(label="read")
-            >> glue
             >> Edge(label="transform")
+            >> glue
+            >> Edge(label="load")
             >> s3_processed
         )  # Transform and Load data with Glue
 
-        (athena >> Edge(label="query") >> s3_processed)
-        athena >> Edge(label="store sample") >> s3_athena_results
+        (
+            s3_processed
+            << Edge(label="query")
+            << athena
+            >> Edge(label="sample query results")
+            >> s3_athena_results
+        )  # Query processed data and store sample results
+
         (
             lambda_function >> Edge(label="validate results") >> s3_athena_results
         )  # Validate results with Lambda
 
-        with Cluster("Monitoring & Security"):
-            cloudwatch = Cloudwatch("CloudWatch\n(Monitoring)")
-            cloudtrail = Cloudtrail("CloudTrail\n(Audit Logs)")
-            iam = IAM("IAM\n(Access Management)")
-            sns = SNS("SNS\n(Notification Service)")
-
-        sns >> Edge(label="alert") >> dev  # Notifications
-
-        terraform >> Edge(label="provision") >> aws_cloud  # Provision AWS resources
+        # Monitoring & Notifications
+        (
+            cloudwatch - sns >> Edge(label="alert") >> dev
+        )  # Email notifications for pipeline failures
---
+++ assets/dev-workflow.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from diagrams import Diagram, Edge
+
+from diagrams.custom import Custom
+from diagrams.onprem.iac import Terraform
+from diagrams.onprem.vcs import Git, Github
+
+# Project directories
+ROOT_DIR = Path(__file__).resolve().parent.parent
+ICONS_DIR = ROOT_DIR / "assets/icons"
+DIAGRAMS_DIR = ROOT_DIR / "assets/diagrams"
+
+with Diagram(
+    "FXLake: Serverless ETL on AWS\n(development workflow)",
+    filename=str(DIAGRAMS_DIR / "dev-workflow"),
+    show=False,
+    direction="LR",
+):
+    # Developer environment
+    dev = Custom("Developer", str(ICONS_DIR / "dev.jpg"))
+    git = Git("Git\n(Version Control)")
+    github = Github("GitHub\n(Repository Hosting)")
+    terraform = Terraform("(Infrastructure as Code)")
+
+    dev >> Edge(label="Develop") >> git  # Developer pushes to version control
+
+    (
+        git >> Edge(label="Pull") >> github >> Edge(label="Push") >> git
+    )  # Version control interaction with GitHub
+
+    git >> Edge(label="Trigger") >> terraform  # Trigger IaC on code push
